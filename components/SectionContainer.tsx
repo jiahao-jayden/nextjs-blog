9e80c8abea594850e624dbaa993b259fc1dd0173
Jayden <2077780946@qq.com>
2026-05-09 01:40:32 +0800

    style: adjust padding in SectionContainer component

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return <section className="mx-auto max-w-3xl px-6 sm:px-8">{children}</section>
+  return <section className="mx-auto max-w-3xl px-4 sm:px-8">{children}</section>
 }
